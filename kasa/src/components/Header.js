e055affc89e187eb38ccc33a86e0bc01b024f001
daiba <daiba_b@yahoo.com>
2023-07-09 01:38:13 +0200

    Ajouts

--- kasa/src/components/Header.js
+++ kasa/src/components/Header.js
@@ -1,7 +1,16 @@
-/*import HeaderLogo from "../assets/HeaderLogo.svg";*/
+import { NavLink } from "react-router-dom";
+import HeaderLogo from "../assets/HeaderLogo.svg";
 
 function Header() {
-  return;
+  return (
+    <header>
+      <img src={HeaderLogo} alt="Logo" />
+      <nav>
+        <a> Accueil </a>
+        <a> À propos </a>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
