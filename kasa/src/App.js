c0ccb7f556390335773d162014985400a25d74d1
daiba <daiba_b@yahoo.com>
2023-07-09 01:25:43 +0200

    Ajouts

--- kasa/src/App.js
+++ kasa/src/App.js
@@ -1,16 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
-import Header from "./pages/About/AboutPage";
-import Header from "./pages/Home/HomePage";
-import Header from "./pages/Logement/LogementPage";
-import Header from "./pages/PageNotFound/PageNotFound";
+import AboutPage from "./pages/About/AboutPage";
+import HomePage from "./pages/Home/HomePage";
+import LogementPage from "./pages/Logement/LogementPage";
+import PageNotFound from "./pages/PageNotFound/PageNotFound";
 
 function App() {
   return (
     <Router>
       <Header />
-
       <Route path="/About" component={AboutPage} />
       <Route path="/Home" component={HomePage} />
       <Route path="/Logement" component={LogementPage} />
