edf641c3da9c0186f105515c01d5f682e81a56bd
daiba <daiba_b@yahoo.com>
2023-07-08 22:26:38 +0200

    Ajouts

--- kasa/src/App.js
+++ kasa/src/App.js
@@ -1,5 +1,13 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Header from "./components/Header";
+
 function App() {
-  return;
+  return (
+    <Router>
+      <Header />
+    </Router>
+  );
 }
 
 export default App;
---
+++ kasa/src/components/Header.js
@@ -0,0 +1,7 @@
+/*import HeaderLogo from "../assets/HeaderLogo.svg";*/
+
+function Header() {
+  return;
+}
+
+export default Header;
