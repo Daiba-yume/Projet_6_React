2a00c5a02b291df713f4c96f6472599cdc3fc1f7
daiba <daiba_b@yahoo.com>
2023-07-11 19:40:18 +0200

    Ajouts

--- kasa/src/components/footer/Footer.jsx
+++ kasa/src/components/footer/Footer.jsx
@@ -1,5 +1,14 @@
+import React from "react";
+import FooterLogo from "../../assets/FooterLogo.svg";
+import "./Footer.scss";
+
 function Footer() {
-  return <footer>c'est le footer</footer>;
+  return (
+    <footer>
+      <img src={FooterLogo} alt="Logo" />
+      c'est le footer
+    </footer>
+  );
 }
 
 export default Footer;
