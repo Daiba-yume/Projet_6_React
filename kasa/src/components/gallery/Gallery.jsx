4dcc4b8700f8d452e1e85048d41506af8fb6329e
daiba <daiba_b@yahoo.com>
2023-08-12 21:17:42 +0200

    Ajustements

--- kasa/src/components/gallery/Gallery.jsx
+++ kasa/src/components/gallery/Gallery.jsx
@@ -6,50 +6,43 @@ import "./Gallery.scss";
 function Gallery({ pictures }) {
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  const handleLeftClick = () => {
-    setCurrentIndex((currentIndex - 1 + pictures.length) % pictures.length);
-  };
-
-  const handleRightClick = () => {
-    setCurrentIndex((currentIndex + 1) % pictures.length);
+  const handleNavigation = (nextIndex) => {
+    const newIndex = (nextIndex + pictures.length) % pictures.length;
+    setCurrentIndex(newIndex);
   };
 
   if (!pictures || pictures.length === 0) {
     return null;
   }
 
-  const showCounter = pictures.length > 1;
-  const isFirstImage = currentIndex === 0;
-  const isLastImage = currentIndex === pictures.length - 1;
+  const showNavigation = pictures.length > 1;
 
   return (
     <div className="gallery">
-      {showCounter && (
-        <div className="counter">
-          {currentIndex + 1} / {pictures.length}
-        </div>
-      )}
-      {!isFirstImage && (
-        <img
-          className="VectorLeft"
-          src={VectorLeft}
-          alt="Précédent"
-          onClick={handleLeftClick}
-        />
+      {showNavigation && (
+        <>
+          <img
+            className="VectorLeft"
+            src={VectorLeft}
+            alt="Précédent"
+            onClick={() => handleNavigation(currentIndex - 1)}
+          />
+          <div className="counter">
+            {currentIndex + 1} / {pictures.length}
+          </div>
+          <img
+            className="VectorRight"
+            src={VectorRight}
+            alt="Suivant"
+            onClick={() => handleNavigation(currentIndex + 1)}
+          />
+        </>
       )}
       <img
         className="currentPicture"
         src={pictures[currentIndex]}
         alt={`Logement ${currentIndex + 1}`}
       />
-      {!isLastImage && (
-        <img
-          className="VectorRight"
-          src={VectorRight}
-          alt="Suivant"
-          onClick={handleRightClick}
-        />
-      )}
     </div>
   );
 }
